5da8845660316df84515ee3ca73078924f7a5fe6
obraluv <obra4luv@hotmail.com>
2024-02-18 13:42:52 +0000

    users' details_input

---
+++ details.py
@@ -0,0 +1,16 @@
+# The following command will get the user's name
+name = input("please enter your name:")
+
+# The following command will get the user's age
+age = input("please enter your age:")
+
+# The following command will get the user's house number
+house_number = input("please enter your house number:")
+
+# The following command will get the user's street number
+street_number = input("please enter your street number:")
+
+# The following command will show a single sentence containing all the user's details
+to_print = f"This is {name}. She is {age} years old and lives at house number {house_number} on {street_number} street"
+
+print(to_print)
