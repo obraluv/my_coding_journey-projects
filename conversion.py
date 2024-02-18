804c717f4d84c9e6b7e1d6ab714747848dfa7953
obraluv <obra4luv@hotmail.com>
2024-02-18 13:27:52 +0000

    Python data types conversion

---
+++ conversion.py
@@ -0,0 +1,20 @@
+#the variable num1 is assigned the float due to the presence of decimal point and lack of quotation marks
+num1 = 99.23
+#converting num1 which is a float into an integer
+number_int = int(num1)
+print(number_int)
+#the variable num2 is assigned the integer due to the presence of digits and lack of quotation marks
+num2 = 23
+#converting num2 which is an integer into a float
+number_float =float(num2)
+print(number_float)
+#the variable num3 is assigned the integer due to the presence of digits and lack of quotation marks
+num3 = 150
+#converting num3 which is an integer into a string
+number_str = str(num3)
+print(number_str)
+#the variable string1 is assigned the string due to the presence of quotation marks
+string1 = "100"
+#converting string1 which is a string into an integer
+number_int2 = int("100")
+print(number_int2)
